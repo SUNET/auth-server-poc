b06e257fd9a05ba29fb2e67be9081160e95847d9
Johan Marcusson <johan.marcusson@sunet.se>
2019-10-09 13:15:06 +0200

    working way to get username

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<p>Environ: {}</p><p>Auth header: {}</p>".format(os.environ['REMOTE_USER'],
-                                                             request.headers.get('Authorization'))
+    return "<p>Username: {}</p><p>Auth type: {}</p>".format(request.environ.get('REMOTE_USER'),
+                                                            request.environ.get('AUTH_TYPE'))
+
